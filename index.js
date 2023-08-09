f1a02e321af36de7a8a6545fee0bf36fff990be0
Mauricio Mourraille <mourraille@me.com>
2023-08-09 11:28:46 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const pino = require('pino');
+const pinoPretty = require('pino-pretty');
+var cron = require('node-cron');
+
+
+//variable declaration
+const url = '<API_TO_MONITOR>'
+
+//logger setup 
+const stream =  pinoPretty({
+    colorize: true
+  })
+const logger = pino ({ level: 'info' }, stream);
+
+
+function fetch() {
+// fetch current slots
+axios({
+    method: 'get',
+    url: url,
+  })
+    .then(function (response) {
+        logger.info(response.data.code + " | " + response.data.status)
+        //Handle payload here
+    });
+}
+// push the data to your telegram
+function telegramPush(message){
+    axios.post('https://api.telegram.org/<YOUR BOT CREDENTIALS HERE>/sendMessage', {
+                    chat_id: '<TELEGRAM_CHAT_ID>',
+                    text: message,
+                    disable_notification: false
+                  })
+                  .then(function (response) {
+                    logger.info(response);
+                  })
+                  .catch(function (error) {
+                    logger.error(error);
+                  });            
+}
+
+fetch() // run it once to make sure it works
+
+cron.schedule('*/20 * * * *', () => { // fetch the API and push results every 20 min
+    fetch()
+});
